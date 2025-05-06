abf81ac8e36fa9b2f01e4181fd24e5fcbf7b1dae
RavimalRanathunga <ravimalranathunga@gmail.com>
2025-05-06 15:16:24 +0530

    resolve errors

--- src/index.js
+++ src/index.js
@@ -3,26 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB3fqwEoe3ZmlvxB4j-On526f6YzIHxiIY",
-  authDomain: "ticketai-80877.firebaseapp.com",
-  projectId: "ticketai-80877",
-  storageBucket: "ticketai-80877.firebasestorage.app",
-  messagingSenderId: "1080117589149",
-  appId: "1:1080117589149:web:9d46c07ef49ee0fad01074",
-  measurementId: "G-LKMRJDPBD3"
-};
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
